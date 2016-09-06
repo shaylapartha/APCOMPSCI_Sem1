8feb1a4b9bc8bc62b34c56a30e66bad268462d49
shaylapartha <shayla.parthasarathy@gmail.com>
2016-09-06 07:52:52 -0700

    Added Files

---
+++ APLesson_01/ASCIIart.java
@@ -0,0 +1,17 @@
+public class ASCIIart
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                              (_)(_) ");
+		System.out.println("                             /     \\ ");
+		System.out.println("                            /       |");
+		System.out.println("                           /   \\  * |");
+		System.out.println("             ________     /    /\\__/ ");
+		System.out.println("     _      /        \\   /    /      ");
+		System.out.println("    / \\    /  ____    \\_/    /       ");
+		System.out.println("   //\\ \\  /  /    \\         /        ");
+		System.out.println("   V  \\ \\/  /      \\       /         ");
+		System.out.println("       \\___/        \\_____/          ");
+
+	}
+}
---
+++ APLesson_01/ASCIIart2.java
@@ -0,0 +1,9 @@
+public class ASCIIart2
+{
+	public static void main(String[]args)
+	{
+		System.out.println("                _      _      _     ");
+		System.out.println("              >(.)__ <(.)__ >(.)__  ");
+		System.out.println("               (___/  (___/  (___/  ");
+	}
+}
