221298a6eabf095229aa24ae1f85ef9c88753cb0
shaylapartha <shayla.parthasarathy@gmail.com>
2016-09-06 09:20:48 -0700

    Added Files

---
+++ APLesson-02/Lab_02.java
@@ -0,0 +1,25 @@
+public class Lab_02
+{
+	public static void main(String[]args)
+	{
+		int x = 2;
+		int y = 4;
+		int z = x * y;
+		
+		System.out.println(x + " multiplied by " + y + " is " + z);
+	}
+}
+{
+	public static void main(String[]args)
+	{
+		int Name = "Shayla Parthasarathy";
+		int Address = "5548 Caminito Vista Lujo";
+		int City = "San Diego";
+		int State = "CA";
+		int ZipCode = 92130;
+		
+		System.out.println(Name);
+		System.out.println(Address);
+		System.out.println(City + " , " + State + " , " + ZipCode);
+	}
+}
---
+++ APLesson-02/Lesson_02example.java
@@ -0,0 +1,8 @@
+public class Lesson_02example
+{
+	public static void main(String[]args)
+	{
+		String name = "ProfessorHandsome";
+		System.out.println(name);
+	}
+}
