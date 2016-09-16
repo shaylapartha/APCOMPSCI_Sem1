3925162b637426174a203bfe6f87c1f9d5be36c7
shaylapartha <shayla.parthasarathy@gmail.com>
2016-09-16 09:20:56 -0700

    fixing it

--- APLesson_04/APLab_04ex_01.java
+++ APLesson_04/APLab_04ex_01.java
@@ -1,20 +1,23 @@
+import java.util.Scanner;
+
 public class APLab_04ex_01
 {
 	public static void main(String[]args)
 	{
+		Scanner = new Scanner(System.in);
 		APLab_04ex_01 form = new APLab_04ex_01();
 		System.out.println("<<<<<<<<<<_Receipt_>>>>>>>>>>");
 		
 		String item1 = "Cheese Sandwich........";
-		double price1 = 4.50;
+		double price1 = kb.nextDouble();
 		form.format(item1, price1);
 		
 		String item2 = "French Fries........";
-		double price2 = 2.88;
+		double price2 = kb.nextDouble;
 		form.format(item2, price2);
 		
 		String item3 = "Soda........";
-		double price3 = 1.63;
+		double price3 = kb.nextDouble();
 		form.format(item3, price3);
 	}
 	
