6a30ff56637e4a2bea6f80fb912842b741cca33d
shaylapartha <shayla.parthasarathy@gmail.com>
2016-09-16 08:23:26 -0700

    Added File

---
+++ APLesson_04/Lesson_04.java
@@ -0,0 +1,8 @@
+public class Lesson_04
+{
+	public static void main(String[]args)
+	{
+		System.out.printf("%10s  %10.2f", "test",  4564.3453);
+		System.out.printf("\n%10s  %10.2f", "thisexam", 909878.00);
+	}
+}
