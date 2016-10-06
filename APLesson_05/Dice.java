a82fedd22f3a5c5a42704a39bb718cbcdf4b9af2
shaylapartha <shayla.parthasarathy@gmail.com>
2016-10-06 09:12:40 -0700

    Added Files

---
+++ APLesson_05/Dice.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+public class Dice
+{
+	public static void main(String[]args)
+	{
+		Random rand = new Random();
+		int yourRoll = rand.nextInt(6)+1;
+		int compRoll = rand.nextInt(6)+1;
+		System.out.println("You rolled a " + yourRoll);
+		System.out.println("Computer rolled a " + compRoll);
+	}
+	
+	public static boolean rollDice()
+	{
+		boolean 
+		if()
+	}
+}
