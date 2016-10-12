f61b50664ff5181908f8754829a75b3493dae570
shaylapartha <shayla.parthasarathy@gmail.com>
2016-10-12 07:56:02 -0700

    Added Files

--- APLesson_05/Receipt.java
+++ APLesson_05/Receipt.java
@@ -4,7 +4,6 @@ public class Receipt
 	public static void main(String[]args)
 	{
 		Scanner kb = new Scanner(System.in);
-		Receipt form = new Receipt();
 		
 		System.out.println("Enter item 1: ");
 		String item1 = kb.next();
@@ -24,18 +23,36 @@ public class Receipt
 		double price4 = kb.nextDouble();
 		
 		double subtotal = (price1 + price2 + price3 + price4);
+		double discount = calcDisc(subtotal);
+		double tax = subtotal * 0.08;
+		double total = subtotal - discount + tax;
 
 		System.out.println("<<<<<<<<<<< Receipt >>>>>>>>>>>");
 		System.out.println("\n");
-		form.format(item1, price1);
-		form.format(item2, price2);
-		form.format(item3, price3);
-		form.format(item4, price4);
+		format(item1, price1);
+		format(item2, price2);
+		format(item3, price3);
+		format(item4, price4);
+		System.out.println("\n");
 		
+		format("Subtotal: ", subtotal);
+		format("Discount: ", discount);
+		format("Tax: ", tax);
+		format("Total: ", total);
+		System.out.println("\n");
+		System.out.println("_________________________________");
+		System.out.println("Thank you for shopping!");
 		
 	}
 	
-	public void format(String item, double price) 
+	public static double calcDisc(double sub)
+	{
+		if (sub >= 2000)
+			return sub *0.15;
+		return 0.0;
+	}
+	
+	public static void format(String item, double price) 
 	{
 		System.out.printf("\n*%15s.........  %15.2f", item, price);
 	}
