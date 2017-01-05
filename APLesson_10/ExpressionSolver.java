9d2b033e544c1151271e475f26af416e90e5dfc1
shaylapartha <shayla.parthasarathy@gmail.com>
2017-01-05 09:12:59 -0800

    added files

---
+++ APLesson_10/ExpressionSolver.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class ExpressionSolver
+{
+	public static void main(String[]args)
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter an equation: ");
+		String expression = kb.nextLine();
+	
+		ArrayList<String>equation = new ArrayList<>(Arrays.asList(expression.split(" ")));
+		doEquation(equation);
+		
+		int i=0;
+		while(i < equation.size())
+		{
+			if(i < equation.size() && equation.get(i).equals("+"))
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) + Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}	
+			else
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) - Integer.parseInt(equation.get(i+1))));
+				i++;
+			}
+		}
+		System.out.println(equation);
+
+	}
+	
+	public static void doEquation(ArrayList<String>equation)
+	{
+		int i=0;
+		while(i < equation.size())
+		{
+			if(i < equation.size() && equation.get(i).equals("*"))
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) * Integer.parseInt(equation.get(i+1))));
+				equation.remove(i-1);
+				equation.remove(i);
+			}
+			else
+			{
+				equation.set(i, "" + (Integer.parseInt(equation.get(i-1)) / Integer.parseInt(equation.get(i+1))));
+				i++;
+			}
+			
+		}
+	}
+}
